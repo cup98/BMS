5ff57a5f90e6f2eec6ad0f80fe6da7527160d0f5
CUP98 <7921709+cup98@user.noreply.gitee.com>
2022-07-18 10:58:12 +0800

    在.gitignore文件中进行推送main.c测试2

---
+++ BMS/Sources/main.c
@@ -0,0 +1,30 @@
+#include <hidef.h>
+#include "derivative.h"
+#include "CAN.h"
+#include "ECT.h"
+#include "PIT.h"
+#include "PLL.h"
+
+#define MODE_ONE      PORTC_PC4
+#define MODE_ONE_DIR  DDRC_DDRC4
+#define MODE_TWO      PORTC_PC5
+#define MODE_TWO_DIR  DDRC_DDRC5
+
+void main(void)
+{
+    DisableInterrupts;
+    MODE_ONE = 1;
+    MODE_ONE_DIR = 1;
+    MODE_TWO = 0;
+    MODE_TWO_DIR = 1;
+    PLL_Init();
+    ECT_Init();
+    CAN_Init();
+    PIT_Init();
+    EnableInterrupts;
+
+    for(;;)
+    {
+        _FEED_COP();
+    }
+}
